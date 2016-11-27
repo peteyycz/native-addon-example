de9a42d2af40a996b116b865cd5a168da69d37a8
Peter Czibik <p.czibik@gmail.com>
2016-11-27 14:18:54 +0100

    rewrite addon with NaN

--- example.cc
+++ example.cc
@@ -1,42 +1,38 @@
-#include <node.h>
+#include <nan.h>
 
 const int maxValue = 10;
 int numberOfCalls = 0;
 
-void WhoAmI(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  v8::Isolate* isolate = args.GetIsolate();
-  auto message = v8::String::NewFromUtf8(isolate, "I'm a Node Hero!");
+void WhoAmI(const Nan::FunctionCallbackInfo<v8::Value>& args) {
+  auto message = Nan::New<v8::String>("I'm a Node Hero!").ToLocalChecked();
   args.GetReturnValue().Set(message);
 }
 
-void Increment(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  v8::Isolate* isolate = args.GetIsolate();
-
+void Increment(const Nan::FunctionCallbackInfo<v8::Value>& args) {
   if (!args[0]->IsNumber()) {
-    isolate->ThrowException(v8::Exception::TypeError(
-          v8::String::NewFromUtf8(isolate, "Argument must be a number")));
+    Nan::ThrowError("Argument must be a number");
     return;
   }
 
   double argsValue = args[0]->NumberValue();
   if (numberOfCalls + argsValue > maxValue) {
-    isolate->ThrowException(v8::Exception::Error(
-          v8::String::NewFromUtf8(isolate, "Over the roof!")));
+    Nan::ThrowError("Counter went through the roof!");
     return;
   }
 
   numberOfCalls += argsValue;
 
   auto currentNumberOfCalls =
-    v8::Number::New(isolate, static_cast<double>(numberOfCalls));
+    Nan::New<v8::Number>(numberOfCalls);
 
   args.GetReturnValue().Set(currentNumberOfCalls);
 }
 
-
 void Initialize(v8::Local<v8::Object> exports) {
-  NODE_SET_METHOD(exports, "whoami", WhoAmI);
-  NODE_SET_METHOD(exports, "increment", Increment);
+  exports->Set(Nan::New("whoami").ToLocalChecked(),
+      Nan::New<v8::FunctionTemplate>(WhoAmI)->GetFunction());
+  exports->Set(Nan::New("increment").ToLocalChecked(),
+      Nan::New<v8::FunctionTemplate>(Increment)->GetFunction());
 }
 
-NODE_MODULE(module_name, Initialize)
+NODE_MODULE(addon, Initialize)
